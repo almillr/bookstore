6efc27c45174f889d362a0d1e5598c9cc15b12bb
“Anita <almillr@gmail.com>
2015-12-19 18:00:51 -0800

    class 3 - lab 1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,20 @@ class BooksController < ApplicationController
     @available_at = Time.now
     @books = Book.all
   end
+  def create
+    @book = book.new(book_params)
+    if @book.save
+      redirect_to @book
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render :edit
+    end
+  end
 end
